b809c70182a7fe70de77f698b4a23d701d0c2243
xovrasok <yuliaruda033@gmail.com>
2022-05-16 19:46:03 +0300

    second input for link

--- src/App.js
+++ src/App.js
@@ -5,35 +5,42 @@ import Sentiment from "sentiment";
 const sentiment = new Sentiment();
 
 function App() {
-  const [phrase, setphrase] = useState("");
-  const [score, setscore] = useState(null);
+  const [phrase, setPhrase] = useState("");
+  const [score, setScore] = useState(null);
+  const [inputOne, setInputOne] = useState("");
+    const [scoreSecond, setScoreSecond] = useState(null);
 
   useEffect(() => {
-    setscore(sentiment.analyze(phrase));
+    setScore(sentiment.analyze(phrase));
   }, [phrase]);
+
+    useEffect(() => {
+        setScoreSecond(sentiment.analyze(inputOne));
+    }, [inputOne]);
+
   return (
     <div className="main">
       <div className="main-container">
         <div className="heading">
           <div className="hamburger">
-            <a href="#analysis" style={{ marginLeft: "15%" }}>
-              <p>Analysis</p>
+            <a href="#analysisText" style={{ marginLeft: "15%" }}>
+              <p>Analysis text</p>
             </a>
-            <a href="" style={{ marginLeft: "9%" }}>
-              <p>About Us</p>
+            <a href="#analysisLink" style={{ marginLeft: "9%" }}>
+              <p>Analysis Link</p>
             </a>
           </div>
           <h3 className="text-heading">Sentiment Analysis</h3>
-          <a href="#analysis">
+          <a href="#analysisText">
             <div className="arrow-down" />
           </a>
         </div>
-        <div className="under-heading" id="analysis">
+        <div className="under-heading" id="analysisText">
           <p> Please enter your text in english for analysis </p>
           <input
             value={phrase}
             className="inputbox"
-            onChange={(e) => setphrase(e.target.value)}
+            onChange={(e) => setPhrase(e.target.value)}
           />
         </div>
 
@@ -50,6 +57,27 @@ function App() {
             ""
           )}
         </div>
+          <div className="under-heading" id="analysisLink">
+          <p> Please insert a link to analyze the content of the site </p>
+          <input
+              value={inputOne}
+              className="inputbox"
+              onChange={(e) => setInputOne(e.target.value)}
+          />
+          </div>
+          <div className="img-container">
+              {scoreSecond ? (
+                  scoreSecond.score === 0 ? (
+                      <p>This text is neutral</p>
+                  ) : scoreSecond.score > 0 ? (
+                      <p>This text is: positive ({scoreSecond.score})</p>
+                  ) : (
+                      <p>This text is: negative ({scoreSecond.score})</p>
+                  )
+              ) : (
+                  ""
+              )}
+          </div>
       </div>
     </div>
   );
